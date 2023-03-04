84d63e59937ef2835a63decff5b73f0cf2365dbe
guanweilu <995300788@qq.com>
2023-03-04 14:53:04 +0800

    补充

--- src/resources/main-vue/src/plugin.js
+++ src/resources/main-vue/src/plugin.js
@@ -6,7 +6,7 @@ export default {
   async install (Vue) {
     Vue.config.productionTip = false;
     Vue.prototype.$lodash = Lodash;
-    Vue.prototype.$clientInterface = param=> {window?.clientInterface(param)};
+    Vue.prototype.$clientInterface = param => window?.clientInterface(param);
     Vue.use(Element, {size: "small", zIndex: 3000});
   }
 };
